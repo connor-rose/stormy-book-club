9004059e7d6f2d16d0ea0d3c50dfa65bb1ebe82b
Connor Rose <connor.rose@icloud.com>
2025-10-27 15:51:51 -0700

    Change icon picker: square (green) for frog, lightning bolt (purple) for elephant

--- script.js
+++ script.js
@@ -248,10 +248,10 @@ class BookManager {
                     </div>
                     <div class="book-icon-picker">
                         <button class="icon-btn ${book.iconType === 'frog' ? 'active' : ''}" data-icon="frog" data-book-id="${book.id}">
-                            <i data-lucide="zap"></i>
+                            <i data-lucide="square"></i>
                         </button>
                         <button class="icon-btn ${book.iconType === 'elephant' ? 'active' : ''}" data-icon="elephant" data-book-id="${book.id}">
-                            <i data-lucide="heart"></i>
+                            <i data-lucide="zap"></i>
                         </button>
                     </div>
                 </div>
